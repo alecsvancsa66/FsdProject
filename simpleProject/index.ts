da7d196cbc15979bffc7dd3bdd32b40b05f1512b
shadowi3lade22 <reculin.george95@gmail.com>
2021-04-19 17:57:32 +0300

    Connect to datastore, added save and list

--- simpleProject/index.ts
+++ simpleProject/index.ts
@@ -1,6 +1,23 @@
+import { Datastore } from '@google-cloud/datastore';
 
-export function helloWorld  (req, res)  {
+const datastore = new Datastore({
+    projectId: 'fsdproject-310513',
+    keyFilename: 'fsdproject-key.json'
+});
 
+const kindName = 'user-log';
+
+export async function helloWorld  (req, res)  {
+
+
+    const query = datastore.createQuery(kindName);
+    const [tasks] = await datastore.runQuery(query);
+    console.log('Tasks:');
+    for (const task of tasks) {
+      const taskKey = task[datastore.KEY];
+      console.log(taskKey.id, task);
+    }
+    
     res.set('Access-Control-Allow-Origin', '*')
 
     if (req.method === "OPTIONS"){
@@ -9,7 +26,27 @@ export function helloWorld  (req, res)  {
         res.set('Access-Control-Max-Age', '3600');
         res.status(204).send('');
     }else{
-        res.send({msg:"Hello world!"})
+        res.send(tasks);
     }
 
   };
+
+ export function saveLog (req, res) {
+    let uid = req.query.uid || req.body.uid || 0;
+    let title = req.body.title || '';
+    let description = req.body.description || '';    
+    datastore.save({
+            key: datastore.key(kindName),
+            data: {
+                title: title,
+                description: description,
+                uid: datastore.int(uid),
+                time_create: datastore.int(Math.floor(new Date().getTime()/1000))
+            }
+        })
+        .catch(err => {
+            console.error('ERROR:', err);
+            res.status(200).send(err);
+            return;
+        });    res.status(200).send({uid,title,description});
+};
